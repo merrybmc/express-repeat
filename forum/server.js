71b1b68788e0aeeaa2b438078663e3dcfdcc2ee9
Byeong Min Cho <merrybmc@gmail.com>
2024-05-04 01:33:55 +0900

    mongodb connect

--- forum/server.js
+++ forum/server.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+const url = process.env.MONGODB_URI;
+
+let db;
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    db = client.db('forum');
+  })
+  .catch((err) => console.log(err));
+
+// express static file
+
+// case 1
+app.use(express.static('public'));
+
+// case 2
+// app.use(express.static(__dirname + '/public'));
 
 // server connect, parameter 1 = 서버 띄울 PORT 번호 입력
 app.listen(8080, () => {
@@ -13,6 +33,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/news', (req, res) => {
+  db.collection('post').insertOne({ title: 'hello' });
   res.send('good news');
 });
 
